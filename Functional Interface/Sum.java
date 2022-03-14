034639efd97dfbc65522d9ef67af20def6976855
Rhea Sidana <37862762+RheaSidana@users.noreply.github.com>
2022-03-14 19:13:01 +0530

    Update Sum.java

--- Functional Interface/Sum.java
+++ Functional Interface/Sum.java
@@ -1,8 +1,4 @@
-public class Test {
-    public static void main(String[] args) {
-        Sum s = (int x, int y) -> System.out.println("Sum: "+ (x+y));
-
-        s.add(3,5);
-        s.add(2,4);
-    }
+@FunctionalInterface
+public interface Sum {
+    void add(int a,int b);
 }
